789fba03bdbc9dda3d4033c269ab6190e9426f55
hellu@Jungle.com <your-new-email@example.com>
2025-03-02 13:22:18 -0800

    Configure Vite and ESlint

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     port: 3000,
   },
 })
