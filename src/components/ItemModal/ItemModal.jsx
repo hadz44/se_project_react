9086426ada0d376dbe99c41e70b492fe98e24e41
hellu@Jungle.com <your-new-email@example.com>
2025-03-16 13:59:08 -0800

    opened image modal

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -2,7 +2,7 @@ import "./Modal.css";
 
 function ItemModal ({activeModal,closeModal, card}) {
    return (
-      <div className={`modal ${activeModal === "preview" && "modal__opened"}`}>
+      <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
         <div className="modal__content modal__content_type_image">
          <button
           onClick={closeModal} 
