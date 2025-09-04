b4cb85bf2d04332d994cbdd3b77993a2c93fc456
hellu@Jungle.com <your-new-email@example.com>
2025-09-04 10:22:04 -0500

    Remove unused WeatherCard import from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
 import "./vendor/fonts.css";
-import WeatherCard from "./components/WeatherCard/WeatherCard";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
